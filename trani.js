bd8d1b8bed2fe8133ba03b50ead6dedaf1d62f97
Aaron Ellis Snyder <aesnyder@gmail.com>
2012-10-22 11:12:50 -0400

    video seeks to clicked sentence

--- trani.js
+++ trani.js
@@ -5,13 +5,21 @@
     var froogInstance = $f(this.attr('id'));
     froogInstance.addEvent('ready', checkTime);
 
+    function jumpToClickedSentance() {
+      $(args).click(function(i) {
+        tmpTime = Number($(this).data("time"));
+        froogInstance.api('seekTo', tmpTime);
+        froogInstance.api('play');
+      });
+    }
+
     function initTimes(args) {
       $(id + "_transcript").find(args).each(function() {
         var self = $(this);
         events[self.data('time')] = self;
       });
     }
-    
+
     function addEvent(element, eventName, callback) {
       $(element).on(eventName, callback);
     }
@@ -19,15 +27,14 @@
     function checkTime() {
       froogInstance.addEvent('playProgress', function(data) {
         var curTime = Math.floor(Number(data.seconds));
-        if (events[curTime]) {
+        if(events[curTime]) {
           $('.highlight').removeClass('highlight');
           events[curTime].addClass('highlight');
         }
       });
     }
-
+    jumpToClickedSentance(args);
     initTimes(args);
     return this;
   }
 })(jQuery);
-
